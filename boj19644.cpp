49021a78bec086c2357af3ba69320323a105fd5e
이강욱 <90788409+2e2guk@users.noreply.github.com>
2022-12-19 10:42:21 +0900

    Update boj19644.cpp

--- boj19644.cpp
+++ boj19644.cpp
@@ -1,12 +1,8 @@
 // boj 19644 좀비 떼가 기관총 진지에도 오다니
-// 안 됨.-2022/12/18
 #include <iostream>
 #include <deque>
 #include <utility>
 #include <climits>
-#include <vector>
-#include <stack>
-
 struct kilo_hecto // 킬로와 헥토 구조체. 좀비가 queue[0]에 도착하면, 죽는다. 
 {
     int is_alive = true;
@@ -25,10 +21,12 @@ struct KM_18A1 // 수평세열지향성지뢰 구조체. 데미지는 INT_MAX으
 {
     int damage = INT_MAX;
     int count = 0;
-    int usage = 0; // 지뢰를 사용한 경우 usage를 올린다. 
 };
 int main()
 {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
     int encampment_length, effective_range, damage, cammo;
     std::cin >> encampment_length;
     //일병 이강욱 등장
@@ -51,117 +49,35 @@ int main()
         std::cin >> zombie_health;
         zombies.push_back({true, zombie_health});
     }
-    // 각 index별 좀비가 사격을 몇 회 당하는지 결정할 vector 선언. 
-    std::vector<int> fire_num;
-    if(K6.effective_range > zombies.size()) // 사격 범위보다 좀비의 수가 더 적은 경우. 그냥 좀비의 현재 (index + 1) 을 기관총 데미지에 곱해서 빼면 된다. 
+    for(int i = 0; i < encampment_length; i++) // 선 사격, 후 덱 오른쪽 회전. 
     {
-        for(int i = 0; i < zombies.size(); i++)
+        if((zombies[0].health - K6.damage) > 0) // 지뢰를 사용해야 하는 경우. 
         {
-            fire_num.push_back(i + 1);
-        }
-        for(int i = 0; i < fire_num.size(); i++)
-        {
-            if((zombies[i].health - (fire_num[i] * K6.damage) > 0) && claymore.count > 0) // 지뢰를 사용할 수 있는 경우. 
+            if(claymore.count > 0)
             {
-                zombies[i].health -= claymore.damage;
-                claymore.count--;
-                claymore.usage++;
-            }
-            else // 지뢰를 사용할 수 없는 경우.
-            {
-                if(claymore.usage <= 0)
-                {
-                    zombies[i].health -= (fire_num[i] * K6.damage);
-                    if(zombies[i].health > 0)
-                    {
-                        std::cout << "NO";
-                        return 0;
-                    }
-                }
-                else if(claymore.usage > 0)
-                {
-                    if(fire_num[i] < K6.effective_range)
-                    {
-                        zombies[i].health -= ((fire_num[i] - claymore.usage) * K6.damage);
-                        if(zombies[i].health > 0)
-                        {
-                            std::cout << "NO";
-                            return 0;
-                        }
-                    }
-                    else
-                    {
-                        zombies[i].health -= (fire_num[i] * K6.damage);
-                        if(zombies[i].health > 0)
-                        {
-                            std::cout << "NO";
-                            return 0;
-                        }
-                    }
-                }
-            }
-        }
-        std::cout << "YES";
-        return 0;
-    }
-    else if(K6.effective_range < zombies.size()) // 사격 범위보다 좀비의 수가 더 많은 경우. 
-    {
-        int max_fire_count = K6.effective_range;
-        int fire_count = 1;
-        for(int i = 0; i < zombies.size(); i++)
-        {
-            if(fire_count < max_fire_count)
-            {
-                fire_num.push_back(fire_count);
-                fire_count++;
-            }
-            if(fire_count == max_fire_count)
-            {
-                fire_num.push_back(fire_count);
-            }
-        }
-        for(int i = 0; i < fire_num.size(); i++)
-        {
-            if((zombies[i].health - (fire_num[i] * K6.damage) > 0) && claymore.count > 0) // 지뢰를 사용할 수 있는 경우. 
-            {
-                zombies[i].health -= claymore.damage;
+                zombies[0].health -= claymore.damage;
                 claymore.count--;
             }
-            if(zombies[i].health - (fire_num[i] * K6.damage) < 0)
+        }
+        else // 사격을 해야 하는 경우. 
+        {
+            for(int i = 0; i < K6.effective_range; i++)
             {
-                if(claymore.usage <= 0)
-                {
-                    zombies[i].health -= (fire_num[i] * K6.damage);
-                    if(zombies[i].health > 0)
-                    {
-                        std::cout << "NO";
-                        return 0;
-                    }
-                }
-                else if(claymore.usage > 0)
-                {
-                    if(fire_num[i] < K6.effective_range)
-                    {
-                        zombies[i].health -= ((fire_num[i] - claymore.usage) * K6.damage);
-                        if(zombies[i].health > 0)
-                        {
-                            std::cout << "NO";
-                            return 0;
-                        }
-                    }
-                    else
-                    {
-                        zombies[i].health -= (fire_num[i] * K6.damage);
-                        if(zombies[i].health > 0)
-                        {
-                            std::cout << "NO";
-                            return 0;
-                        }
-                    }
-                } 
+                zombies[i].health -= K6.damage;
             }
         }
-        std::cout << "YES";
-        return 0;
+        if(zombies[0].health > 0)
+        {
+            std::cout << "NO";
+            return 0;
+        }
+        else // 덱을 오른쪽으로 회전시킨다. 
+        {
+            zombie container = zombies.front();
+            zombies.pop_front();
+            zombies.push_back(container);
+        }
     }
+    std::cout << "YES";
+    return 0;
 }
